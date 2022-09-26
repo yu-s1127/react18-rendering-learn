67baefb9a14fda88269b91893903b95d2904d461
yuhei_suzuki <yooopopo5@gmail.com>
2022-09-26 12:09:52 +0900

    transitionの利用

--- src/components/primes/PrimeNumbers.tsx
+++ src/components/primes/PrimeNumbers.tsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { CheckNumber } from './CheckNumber';
 import { PrimeRange } from './PrimeRange';
 
 const defaultValue = 250;
 
 export function PrimeNumbers() {
+  const [isPending, startTransition] = useTransition();
   const [maxPrime, setMaxPrime] = useState(defaultValue);
   const values = new Array(maxPrime).fill(null);
 
   return (
-    <div className="row">
-      <h2 className="text-center mt-5">Prime Numbers</h2>
+    <div className='row'>
+      <h2 className='text-center mt-5'>Prime Numbers</h2>
       <PrimeRange
         defaultValue={defaultValue}
-        onChange={(value) => setMaxPrime(value)}
+        onChange={(value) => startTransition(() => setMaxPrime(value))}
       />
 
-      <div className="row row-cols-auto g-2">
+      <div className='row row-cols-auto g-2'>
         {values
           .filter((_, index) => index < 10_000)
           .map((_, index) => {
-            return <CheckNumber key={index} value={maxPrime - index} />;
+            return (
+              <CheckNumber
+                key={index}
+                value={maxPrime - index}
+                isPending={isPending}
+              />
+            );
           })}
       </div>
     </div>
